78f030b8123d3cc220c950e1f83cc1963ef01344
AugustoMachado01 <augustomachadochieno01@gmail.com>
2025-02-02 00:20:51 +0100

    chore: port config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP Server Running!')
-})
+app.listen(
+  { port: env.PORT, host: '0.0.0.0' },
+  (err: Error | null, address: string) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`Servidor rodando em ${address}`)
+  }
+)
